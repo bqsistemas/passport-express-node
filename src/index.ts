c18a9d4123593d35f50fd1957cafd839b7133991
Jhon Barrantes <jbarrantes@board.pe>
2022-06-29 18:44:21 -0500

    fix: adjustment configs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 import app from './app'
+import createDebug from 'debug'
+
+// initializations
+const debug = createDebug('app:index')
 
 app.listen(app.get('port'))
+debug(`Server listening to port ${app.get('port')} 🚀!`)
