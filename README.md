be651ff9b98ccc7c05f10f907619aab54a89dd2a
Jhon Barrantes <jbarrantes@board.pe>
2022-06-29 18:35:53 -0500

    Feat: Added configs

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # passport-express-node
+https://www.youtube.com/watch?v=eU-p-jreAN4
+
+# comandos
+-- registrar el archivo de configuración para typescript
+npx tsc --init
---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+import morgan from 'morgan'
+import createDebug from 'debug'
+import cors from 'cors'
+
+// initializations
+const debug = createDebug('app')
+const app = express()
+
+// settings
+app.set('port', process.env.PORT || 3000)
+
+// middlewares
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// routes
+app.get('/', (req, res) => {
+    res.send(`The API is at http://localhost:${app.get('port')}`)
+})
+
+export default app
